1fc82fa8c1a47d80308fb81c31396f76753c1ed2
OttoRobba <otto@ottorobba.com>
2014-01-06 10:30:43 -0200

    new file to build the snippets into a single xml file

---
+++ src/snippets/build_snippets.lua
@@ -0,0 +1,49 @@
+xmlstart = "<?xml version='1.0' encoding='utf-8'?>\n<snippets language=\"lua\">\n"
+xmlend = "</snippets>"
+
+local file = io.open("love.CALLBACKS.xml", "r")
+callbacks = file:read("*a")
+file:close()
+
+local file = io.open("love.GENERIC.xml", "r")
+generic = file:read("*a")
+file:close()
+
+local file = io.open("love.graphics.COORDINATE.xml", "r")
+coordinate = file:read("*a")
+file:close()
+
+local file = io.open("love.graphics.DRAWING.xml", "r")
+drawing = file:read("*a")
+file:close()
+
+local file = io.open("love.graphics.OCREATION.xml", "r")
+ocreation = file:read("*a")
+file:close()
+
+local file = io.open("love.graphics.STATE.xml", "r")
+state = file:read("*a")
+file:close()
+
+local file = io.open("love.graphics.WINDOW.xml", "r")
+window = file:read("*a")
+file:close()
+
+local file = io.open("love.JOYSTICK.xml", "r")
+joystick = file:read("*a")
+file:close()
+
+
+
+local file = io.open("../../compiled.xml", "w")
+file:write(xmlstart)
+file:write(callbacks)
+file:write(generic)
+file:write(ocreation)
+file:write(coordinate)
+file:write(drawing)
+file:write(state)
+file:write(joystick)
+file:write(window)
+file:write(xmlend)
+file:close()
